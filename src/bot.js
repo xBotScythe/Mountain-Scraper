bb3587b7b54d32acc1d95badd6bc00b93d7a29bd
Noah <52842506+xBotScythe@users.noreply.github.com>
2025-04-13 01:41:53 -0400

    first commit

---
+++ src/bot.js
@@ -0,0 +1,22 @@
+const {Client, Collection} = require('discord.js') // requires discord.js
+require('dotenv').config(); // imports token
+const {token} = process.env;
+const fs = require('fs');
+
+const client = new Client({intents: []});
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync('./src/functions');
+for(const folder of functionFolders)
+{
+    const functionFiles = fs.readdirSync(`/src/function/${folder}`).filter((file) => file.endsWith(".js"));
+    for(const file of functionFiles) requires(`./functions/${folder}/${file}`(client)) 
+}
+
+// sets up handlers for events and commands
+client.handleEvents();
+client.handleCommands();
+
+// logs in with token!
+client.login(token);
